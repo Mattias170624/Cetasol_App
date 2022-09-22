939057821ef92954457818c7f2c05cbae818001b
Mattias170624 <ExozTic_170624@outlook.com>
2022-09-22 09:51:19 +0200

    Added style to startpage for Android.

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,15 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:io';
 
+import 'package:cetasol_app/Widgets/LoginScreen/login_header.dart';
+import 'package:cetasol_app/Widgets/LoginScreen/login_input_fields.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double padding = MediaQuery.of(context).padding.top;
+    double deviceHeight = MediaQuery.of(context).size.height - padding;
+
     return Platform.isAndroid
         ? Scaffold(
-            body: Center(
-              child: Text('test'),
+            //backgroundColor: Colors.transparent,
+            body: SafeArea(
+              child: SingleChildScrollView(
+                physics: const ClampingScrollPhysics(),
+                child: Container(
+                  color: Theme.of(context).colorScheme.primary,
+                  height: deviceHeight,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: LoginHeader(),
+                      ),
+                      Expanded(
+                        child: LoginInputFields(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           )
         : CupertinoPageScaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:io';
 
 import 'package:cetasol_app/Screens/login_screen.dart';
@@ -14,19 +16,39 @@ class MyApp extends StatelessWidget {
     return Platform.isAndroid
         ? MaterialApp(
             theme: ThemeData(
+              scaffoldBackgroundColor: Color(0xFF143452),
               colorScheme: const ColorScheme(
-                  brightness: Brightness.light,
-                  error: Colors.black,
-                  onPrimary: Colors.black,
-                  onError: Colors.black,
-                  onSurface: Colors.black,
-                  onBackground: Colors.black,
-                  primary: Color(0xFF143452),
-                  secondary: Color(0xFF8AC1EA),
-                  surface: Colors.black,
-                  onSecondary: Colors.black,
-                  background: Colors.black),
+                brightness: Brightness.light,
+                error: Colors.black,
+                onPrimary: Color(0xFFFF4d00),
+                onError: Colors.black,
+                onSurface: Colors.black,
+                onBackground: Colors.black,
+                primary: Color(0xFF143452),
+                secondary: Color(0xFF8AC1EA),
+                surface: Colors.black,
+                onSecondary: Colors.black,
+                background: Colors.black,
+              ),
+              textTheme: const TextTheme(
+                titleLarge: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+                titleMedium: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+                titleSmall: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
             ),
+            color: Colors.red,
             home: LoginScreen(),
           )
         : CupertinoApp(
